41b6358ab7e25e69ed354d42d9680f3f15ee7506
boyrazcan33 <ugurboy94@gmail.com>
2025-07-14 14:39:09 +0300

    OSMR fixed

--- route/src/main/java/com/app/route/util/RealDistanceCalculator.java
+++ route/src/main/java/com/app/route/util/RealDistanceCalculator.java
@@ -1,5 +1,6 @@
 package com.app.route.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -7,16 +8,24 @@ import com.fasterxml.jackson.databind.JsonNode;
 @Component
 public class RealDistanceCalculator {
 
+    @Value("${osrm.api.base-url}")
+    private String osrmBaseUrl;
+
     private final RestTemplate restTemplate = new RestTemplate();
 
     public double getRoadDistance(double lat1, double lon1, double lat2, double lon2) {
         String url = String.format(
-                "http://router.project-osrm.org/route/v1/driving/%f,%f;%f,%f?overview=false",
-                lon1, lat1, lon2, lat2
+                "%s/route/v1/driving/%f,%f;%f,%f?overview=false",
+                osrmBaseUrl, lon1, lat1, lon2, lat2
         );
 
         try {
             JsonNode response = restTemplate.getForObject(url, JsonNode.class);
+
+            if (response == null || !response.has("routes") || response.get("routes").size() == 0) {
+                throw new RuntimeException("No route found between coordinates");
+            }
+
             double distanceMeters = response.get("routes").get(0).get("distance").asDouble();
             return distanceMeters / 1000; // Convert to kilometers
         } catch (Exception e) {
