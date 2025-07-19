f9379a6d0cbdc0c4df1854eabd418e39b7b332c3
boyrazcan33 <ugurboy94@gmail.com>
2025-07-19 03:00:13 +0300

    dto2

--- route/src/main/java/com/app/route/dto/RouteStop.java
+++ route/src/main/java/com/app/route/dto/RouteStop.java
@@ -1,4 +1,18 @@
 package com.app.route.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RouteStop {
+    private int order;
+    private String name;
+    private String address;
+    private String description;
+    private int estimatedDuration;
+    private String walkingTime;
+    private double cost;
 }
