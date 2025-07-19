d25e906bb3bcbc58d94811ca21aed44cdcd0fb1c
boyrazcan33 <ugurboy94@gmail.com>
2025-07-19 03:02:49 +0300

    dto3

--- route/src/main/java/com/app/route/dto/RouteResponse.java
+++ route/src/main/java/com/app/route/dto/RouteResponse.java
@@ -1,4 +1,16 @@
 package com.app.route.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RouteResponse {
+    private List<RouteStop> optimizedRoute;
+    private String totalDistance;
+    private String totalDuration;
+    private double totalCost;
 }
